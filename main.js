9e2f0ab492f4f4d5d38bbbc5c75420373254c930
SyntaxErr00 <71900444+SyntaxErr00@users.noreply.github.com>
2020-10-29 17:50:14 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,114 @@
+let str = `<h4>Esperienze</h4>
+<h5>Elettricista</h5>
+<p>Dal 30/05/16 al 19/06/16 presso Elettrocentro di jesi AN (stage)</p>
+<h4>Istruzione e formazione</h4>
+<p>Diploma di maturità perito tecnico elettronico/elettrotecnico  presso ITIS G. MARCONI (jesi AN)</p>
+<h4>lingue</h4>
+<ul>
+  <li>Italiano (madrelingua)</li>
+  <li>Inglese (buono)</li>
+</ul>
+<h4>Competenze</h4>
+<ul>
+  <li>Cablaggio di impianti civile.</li>
+  <li>Lettura schemi elettrici</li>
+  <li>Discrete capacità di programmazione in: C , JAVASCRIPT, HTML/CSS e PYTHON.</li>
+  <li>Conoscenza pacchetto Microsoft office.</li>
+</ul>
+<h4>Attualmente</h4>
+<ul>
+  <li>iscritto presso UNIVPM ingegneria elettronica</li>
+  <li>Iscritto su start2impact.it corso JUNIOR FULL STACK DEVELOPER </li>
+</ul>`;
+
+
+//function declaretion
+function copy(id){
+  let el = document.getElementById(id);
+  let textCopy = document.createElement("input");
+  textCopy.style.display = "hidden";
+  document.body.appendChild(textCopy);
+  textCopy.value = el.text;
+  textCopy.select();
+  textCopy.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  alert("Copied the text: " + textCopy.value);
+  document.body.removeChild(textCopy);
+}
+
+
+
+let isTag, text, i = 0;
+function type(){
+  text = str.slice(0, ++i);
+  if (text === str) return;
+  document.getElementById("ctn").innerHTML = text;
+  let char = text.slice(-1);
+  if(char === '<') isTag = true;
+  if(char === '>') isTag = false;
+
+  if (isTag) return type();
+  window.scrollTo({
+    top:   document.getElementById("ctn").getBoundingClientRect().height,
+    behavior: 'smooth'
+  });
+  setTimeout(type, 50);
+}
+
+//end function declaration
+
+
+
+
+
+
+//copy buttons
+document.querySelector("#emailBtn").addEventListener("click", function() {copy("email");});
+document.querySelector("#phoneBtn").addEventListener("click", function() {copy("phone");});
+
+
+
+//About me button
+let btn = document.querySelector("#btn");
+btn.addEventListener("click", function(){
+  document.getElementById("ctn").classList.toggle("show");
+  type();
+  window.scrollTo({
+    top: btn.getBoundingClientRect().y + window.pageYOffset,
+    behavior: 'smooth'
+  });
+});
+
+//projects button
+let btn2= document.querySelector("#btn2");
+btn2.addEventListener("click", function(){
+  document.getElementById("ctn2").classList.toggle("show");
+  window.scrollTo({
+    top: btn2.getBoundingClientRect().y + window.pageYOffset,
+    behavior: 'smooth'
+  });
+});
+
+//contacts button
+let btn3= document.querySelector("#btn3");
+btn3.addEventListener("click", function(){
+  document.getElementById("ctn3").classList.toggle("show");
+  window.scrollTo({
+    top: btn3.getBoundingClientRect().y + window.pageYOffset,
+    behavior: 'smooth'
+  });
+});
+
+// Close the dropdown menu
+window.onclick = function(event) {
+if (!event.target.matches('.dropbtn')) {
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  var i;
+  for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
